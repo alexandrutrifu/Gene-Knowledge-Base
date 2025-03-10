0e98f257d490b61cd90f766a018f32642c309e3c
Alexandru Trifu-Urzica <alex@Alexandrus-MacBook-Pro-2.local>
2025-03-10 23:36:36 +0200

    added hovel tooltips

--- process_db.py
+++ process_db.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from bokeh.plotting import figure, output_file, show
 from bokeh.embed import components
+from bokeh.models import ColumnDataSource, HoverTool
 
 
 def generate_plot():
@@ -13,13 +14,32 @@ def generate_plot():
 	cursor = con.cursor()
 
 	df = pd.read_sql_query("SELECT * FROM s4b_limma", con)
-	print(df)
 
-	p = figure(title="Volcano plot", x_axis_label='log2 fold change', y_axis_label='-log10 p-value')
+	# Get plot data column
+	data = {
+		'Gene': df['EntrezGeneSymbol'],
+		'logFC': df['logFC'],
+		'adjPVal': -1 * df['adj.P.Val'].apply(np.log10)
+	}
 
-	p.scatter(df['logFC'], -1 * df['adj.P.Val'].apply(np.log10), size=5, marker='circle', color='blue')
+	source = ColumnDataSource(data)
+
+	p = figure(title="Volcano plot", x_axis_label='log2 fold change', y_axis_label='-log10 p-value',
+			   width=800, height=800)
+
+	p.scatter(x='logFC', y='adjPVal', source=source,
+			  size=5, marker='circle', color='blue')
 	p.background_fill_color = None
 	p.border_fill_color = None
+
+	hover = HoverTool()
+	hover.tooltips = [
+		("Gene", "@Gene"),
+		("logFC", "@logFC"),
+		("adj.P.Val", "@adjPVal")
+	]
+
+	p.add_tools(hover)
 
 	script, div = components(p)
 
