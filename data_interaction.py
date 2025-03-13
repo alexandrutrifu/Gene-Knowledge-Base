2084fb9acb437e4270ba23005590c186914e7315
Alexandru Trifu-Urzica <alex@Alexandrus-MacBook-Pro-2.local>
2025-03-13 17:06:47 +0200

    added get_donor_data()

--- data_interaction.py
+++ data_interaction.py
@@ -1,6 +1,7 @@
 import sqlite3
 import pandas as pd
 import numpy as np
+import re
 
 def load_js_callback(filename):
 	with open(filename, 'r') as file:
@@ -13,13 +14,36 @@ def get_dataframe(table_name):
 	cursor = con.cursor()
 	df = pd.read_sql_query(f"SELECT * FROM {table_name}", con)
 
-	# Adjust the p-value to be negative log10
-	df['adj.P.Val'] = -1 * df['adj.P.Val'].apply(np.log10)
-
-	# Rename problematic column name
-	df.rename(columns={'adj.P.Val': 'adj_P_Val'}, inplace=True)
-
 	return df
 
-def get_donor_data(gene_name):
-	pass
+def get_donor_data(df, gene_name):
+	""" Returns protein concentration values across provided donor samples.
+
+	:param df: Dataframe to get donor samples from.
+	:param gene_name: Gene for which to inspect the samples.
+	:return: Lists of concentration values for young/old donors.
+	"""
+	column_names = list(df)
+
+	# Regular expressions for young/old donor column names
+	young_re = re.compile("Set.*\\.YD.*")
+	old_re = re.compile("Set.*\\.OD.*")
+
+	# Get column names encoding young/old donor samples
+	young_sample_keys = filter(lambda name: young_re.match(name) is not None, column_names)
+	old_sample_keys = filter(lambda name: old_re.match(name) is not None, column_names)
+
+	yd_values = []
+	od_values = []
+
+	row_index = np.where(df["EntrezGeneSymbol"] == gene_name)[0][0]
+
+	# Get YD concentration values
+	for yd_sample in young_sample_keys:
+		yd_values.append(df[yd_sample][row_index])
+
+	# Get OD concentration values
+	for od_sample in old_sample_keys:
+		od_values.append(df[od_sample][row_index])
+
+	return yd_values, od_values
