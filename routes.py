cc2fa690a8f74051ab958972745f346098630bdb
Alexandru Trifu-Urzica <alex@Alexandrus-MacBook-Pro-2.local>
2025-03-13 16:30:33 +0200

    adapted to name changes

--- routes.py
+++ routes.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, render_template
-from process_db import generate_plot
+from volcano_plot import generate_volcano_plot
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def index():
-	script, div = generate_plot()
+	script, div = generate_volcano_plot()
 
 	return render_template('home.html', script=script, div=div)
