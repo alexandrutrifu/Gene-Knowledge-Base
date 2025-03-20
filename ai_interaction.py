c5b5e36bf97a875b04add14430bcd2c96bcd652b
Alexandru Trifu-Urzica <alex@Alexandrus-MacBook-Pro-2.local>
2025-03-20 13:20:34 +0200

    reformatted openai call method

--- ai_interaction.py
+++ ai_interaction.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 from os import environ
-
+from gene_requests import get_pubmed_references
 
 PROMPT1 = """
         You are given different datasets including information about protein activity levels across different age groups.
@@ -23,26 +23,47 @@ PROMPT2 = """
         Regulation level: <<upregulated/downregulated/normal>> - <<Short description of its effect>>
         Significance level: <<value>> - <<meaning>>
         """
+PROMPT_PUBMED = """
+            You are given a list of JSON objects containing PUBMED article references in which a given Gene is mentioned.
+            
+            You must read the text field of the dictionary objects (modelling references) and extract a short, meaningful conclusion about the gene's importance and behavioural influences.
+            To do this, select 10 most relevant articles, print their names and use them as reference for your conclusion.
+            
+            You can format the response as a bullet list of short conclusions, redacted in a helpful, accessible tone.
+            """
+
+
 USER_REQUEST_CONTENT = """
                 Gene: Growth/differentiation factor 15
                 logFC: 0.8737907642484315
                 adjPVal: 7.184809761833215
                 """
 
-client = OpenAI(api_key=environ['OPENAI_API_KEY'])
+def callOpenAI(dev_prompt, user_request):
+    """
 
-completion = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-        {
-            "role": "developer",
-            "content": PROMPT2
-        },
-        {
-            "role": "user",
-            "content": USER_REQUEST_CONTENT
-        }
-    ]
-)
+    :param dev_prompt: Prompt to instruct AI on the desired response format.
+    :param user_request: Request to fetch data for.
+    :return: Open AI generated response
+    """
+    client = OpenAI(api_key=environ['OPENAI_API_KEY'])
 
-print(completion.choices[0].message.content)
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "developer",
+                "content": dev_prompt
+            },
+            {
+                "role": "user",
+                "content": user_request
+            }
+        ]
+    )
+
+    response = completion.choices[0].message.content
+
+    print(response)
+
+    return response
