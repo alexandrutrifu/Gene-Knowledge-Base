e85fe2ecb65aadf845bfe9cce9fda96790d51df4
Alexandru Trifu-Urzica <alex@Alexandrus-MacBook-Pro-2.local>
2025-03-20 15:47:07 +0200

    adapted response to streaming format

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 import numpy as np
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, stream_with_context, Response
 
 from ai_interaction import callOpenAI, PROMPT_PUBMED
 from gene_requests import get_pubmed_references
@@ -12,16 +12,16 @@ main_bp = Blueprint('main', __name__)
 @main_bp.route('/')
 def index():
 	# Get dataframes from gene database
-	s4a_df = get_dataframe("s4a_values")
-	s4b_df = get_dataframe("s4b_limma")
+	df_values = get_dataframe("s4a_values")
+	df_limma = get_dataframe("s4b_limma")
 
 	# Adjust the p-value to be negative log10
-	s4b_df['adj.P.Val'] = -1 * s4b_df['adj.P.Val'].apply(np.log10)
+	df_limma['adj.P.Val'] = -1 * df_limma['adj.P.Val'].apply(np.log10)
 
 	# Rename problematic column name
-	s4b_df.rename(columns={'adj.P.Val': 'adj_P_Val'}, inplace=True)
+	df_limma.rename(columns={'adj.P.Val': 'adj_P_Val'}, inplace=True)
 
-	script, div = generate_volcano_plot(s4b_df)
+	script, div = generate_volcano_plot(df_values, df_limma)
 
 	return render_template('home.html', script=script, div=div)
 
@@ -30,4 +30,11 @@ def get_gene_info(gene_id):
 	gene_references = str(get_pubmed_references(gene_id))
 
 	# Call OpenAI API to request information
-	response = callOpenAI(dev_prompt=PROMPT_PUBMED, user_request=gene_references)
+	response = Response(
+		stream_with_context(
+			callOpenAI(dev_prompt=PROMPT_PUBMED, user_request=gene_references)
+		),
+		content_type="text/plain"
+	)
+
+	return response
