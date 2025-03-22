ab62688d55fe01c680a4753e8486e762156e0545
Alexandru Trifu-Urzica <alex@Alexandrus-MacBook-Pro-2.local>
2025-03-22 20:47:22 +0200

    added scroll-down arrow animations

---
+++ static/js/arrow-bounce.js
@@ -0,0 +1,52 @@
+let is_moving = false;
+
+async function addAnimation(arrow, animation_class) {
+	return new Promise((resolve) => {
+		// Remove residual classNames to start properly
+		arrow.classList.remove("move-down", "move-up");
+
+		arrow.classList.add(animation_class);
+
+		// Define animation-end handler
+		function handleAnimationEnd() {
+			arrow.removeEventListener("animationend", handleAnimationEnd);
+			resolve();
+		}
+
+		arrow.addEventListener("animationend", handleAnimationEnd);
+	});
+}
+
+async function startArrowBounceLoop(arrow) {
+	is_moving = true;
+
+	while (is_moving) {
+		await addAnimation(arrow, "move-down");
+		await new Promise((resolve) => setTimeout(resolve, 200));
+		await addAnimation(arrow, "move-up");
+		await new Promise((resolve) => setTimeout(resolve, 400));
+	}
+}
+
+export function animateArrow(arrow) {
+	const observer_options = {
+		root: null,
+		threshold: 0.5
+	};
+
+	const observer = new IntersectionObserver(entries => {
+		for (const entry of entries) {
+			if (entry.isIntersecting) {
+				console.log("happening");
+				if (!is_moving) {
+					startArrowBounceLoop(arrow);
+				}
+			} else {
+				// If arrow is not visible, stop animation
+				is_moving = false;
+			}
+		}
+	}, observer_options);
+
+	observer.observe(arrow);
+}
